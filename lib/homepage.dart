7b091cd71ef31206508f41404b29635255f2f1cc
jasim506022 <jasimrony50@gmail.com>
2023-02-04 12:28:29 +0600

    day3complete

---
+++ lib/homepage.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  final int day = 30;
+  final String name = "Codepur";
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      drawer: Drawer(),
+      body: Center(
+        child: Container(
+          child: Text("Welcome to $day days of flutter of $name"),
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_catelogs_app/homepage.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,11 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Material(
-        child: Container(
-          child: Text("Hellow"),
-        ),
-      ),
+      home: HomePage()
     );
   }
 }
